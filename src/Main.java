83b49bcc80783bd06c95fd795e8a23d8e50feb78
Louis Clark <lclark@softwareinstitute>
2021-11-11 13:59:59 +0000

    reads photo resizes and saves

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -7,15 +8,18 @@ import javax.imageio.ImageIO;
 
 public class Main {
 
-    public static void main(String[] args)
+    public static void main(String[] args){
+            int imageWidth = 1080;
+            int imageHeight = 1080;
 
-    {
+
         URL url = null;
 
-        BufferedImage image = null;
+        BufferedImage originalImage = null;
 
         try
         {
+            //url = new URL("https://images.fasosites.com/13458_2992719m.jpg?cv=201801231221error");
             url = new URL("https://images.fasosites.com/13458_2992719m.jpg?cv=201801231221error");
         }
         catch (MalformedURLException e1)
@@ -25,15 +29,20 @@ public class Main {
 
         try
         {
-            image = ImageIO.read(url);
+            originalImage = ImageIO.read(url);
 
-            ImageIO.write(image, "jpg", new File("C:/Users/louis/Desktop/test/output.jpg"));
+            Image newResizedImage = originalImage
+                    .getScaledInstance(imageWidth, imageHeight, Image.SCALE_SMOOTH);
 
-            ImageIO.write(image, "png", new File("C:/Users/louis/Desktop/test/output.jpg"));
 
-            ImageIO.write(image, "gif", new File("C:/Users/louis/Desktop/test/output.jpg"));
 
-            ImageIO.write(image, "bmp", new File("C:/Users/louis/Desktop/test/output.jpg"));
+            ImageIO.write(convertToBufferedImage(newResizedImage), "jpg", new File("C:/Users/louis/Desktop/test/output.jpg"));
+
+            ImageIO.write(convertToBufferedImage(newResizedImage), "png", new File("C:/Users/louis/Desktop/test/output.jpg"));
+
+            ImageIO.write(convertToBufferedImage(newResizedImage), "gif", new File("C:/Users/louis/Desktop/test/output.jpg"));
+
+            ImageIO.write(convertToBufferedImage(newResizedImage), "bmp", new File("C:/Users/louis/Desktop/test/output.jpg"));
         }
         catch (IOException e)
         {
@@ -41,5 +50,24 @@ public class Main {
         }
 
         System.out.println("done");
+
+    }
+
+    public static BufferedImage convertToBufferedImage(Image img) {
+
+        if (img instanceof BufferedImage) {
+            return (BufferedImage) img;
+        }
+
+        // Create a buffered image with transparency
+        BufferedImage bi = new BufferedImage(
+                img.getWidth(null), img.getHeight(null),
+                BufferedImage.TYPE_INT_ARGB);
+
+        Graphics2D graphics2D = bi.createGraphics();
+        graphics2D.drawImage(img, 0, 0, null);
+        graphics2D.dispose();
+
+        return bi;
     }
 }
